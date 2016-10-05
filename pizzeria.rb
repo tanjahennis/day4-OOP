ad7fb70d73bbafba358e247af898b50d764f89e6
YOUR NAME <tmhennis@gmail.com>
2016-10-05 10:23:40 +0200

    choices

--- pizzeria.rb
+++ pizzeria.rb
@@ -1,5 +1,5 @@
 require './waiter'
 
 w = Waiter.new
-
 w.greet_guest
+w.serve_guests
--- waiter.rb
+++ waiter.rb
@@ -2,4 +2,25 @@ class Waiter
   def greet_guest
     p "Good day! Welcome to our lovely restaurant"
   end
-end
+
+  def serve_guests
+    p "How can I be of service?"
+    p "1. Would you like to order a pizza?"
+    p "2. Would you like to leave?"
+
+    take_order(gets.chomp.to_i)
+  end
+
+  def take_order(order_number)
+    case order_number
+      when 1
+        p "Let me get the menu"
+      when 2
+        p "Thank you for your visit"
+      else
+        p "I don't understand"
+    end
+
+  end
+
+end #end off waiter class
